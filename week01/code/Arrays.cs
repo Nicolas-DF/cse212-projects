5996649c61a77a54f394b26f0f1f6013c0e2895b
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-01-09 09:49:31 -0300

    Implemented the MultiplesOf() method

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,12 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length]; // creating the dinamic array to hold the multiples
+        for (int i=1; i <= length; i++) // loop through from 1 to length usin "i" as the counter AND the number to multiply by
+        {
+            multiples[i-1] = number * i; 
+        }
+        return multiples;
     }
 
     /// <summary>
