aff1c8f54e3e11216ace1ebdbdefbc4123ac053a
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-01-20 19:23:15 -0300

    Implemented the DisplaySumPairs() method

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -28,6 +28,15 @@
     /// </summary>
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
-    }
+        var seenNumbers = new HashSet<int>();
+        foreach (var number in numbers)
+        {
+            int complement = 10 - number;
+            if (seenNumbers.Contains(complement))
+            {
+                Console.WriteLine($"{number} {complement}");
+            }
+            seenNumbers.Add(number);
+        }
+    }   
 }
