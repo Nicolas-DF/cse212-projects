b9a62cad36e83cbc7617d6fe4aaf75aaebdebdb9
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-01-21 09:36:25 -0300

    Implemented Problem 2 - SummarizeDegrees()

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -56,9 +56,16 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
-
         return degrees;
     }
 
