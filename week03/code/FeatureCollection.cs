20552de88e8c0865332942dbef55ecce9dcbcc17
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-01-22 10:34:54 -0300

    Implemented the Features and Properties, along with the necessary methods

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,50 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public Feature[] Features { get; set; }
+    public class Feature
+    {
+        public Properties Properties { get; set; }
+    }
+    public class Properties
+    {
+        public string Place { get; set; }
+        public double Mag { get; set; }
+    }
+
+
+    public double AverageMagnitude()
+    {
+        double total = 0;
+        foreach (var feature in Features)
+        {
+            total += feature.Properties.Mag;
+        }
+        return total / Features.Length;
+    }
+    public string StrongestEarthquakeLocation()
+    {
+        double maxMag = double.MinValue;
+        string location = "";
+        foreach (var feature in Features)
+        {
+            if (feature.Properties.Mag > maxMag)
+            {
+                maxMag = feature.Properties.Mag;
+                location = feature.Properties.Place;
+            }
+        }
+        return location;
+    }
+    public int CountStrongEarthquakes(double threshold)
+    {
+        int count = 0;
+        foreach (var feature in Features)
+        {
+            if (feature.Properties.Mag >= threshold)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
