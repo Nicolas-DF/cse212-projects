0593de3776fe9408725abd765c5150121b47aaf0
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-02-05 09:23:44 -0300

    Implemented the Problem 2

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -40,7 +40,17 @@ public static class Recursion
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            string newWord = word + letters[i];
+            string remainingLetters = letters.Substring(0, i) + letters.Substring(i + 1);
+            PermutationsChoose(results, remainingLetters, size, newWord);
+        }
     }
 
     /// <summary>
