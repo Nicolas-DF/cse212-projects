777b47a3dfc031ad4ae961b16f6b468f5ce0cfbe
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-02-05 10:03:23 -0300

    Implemented Problem 4

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -139,7 +139,17 @@ public static class Recursion
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        if (!pattern.Contains('*'))
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        int index = pattern.IndexOf('*');
+        string patternWithZero = pattern.Substring(0, index) + '0' + pattern.Substring(index+1);
+        string patternWithOne = pattern.Substring(0, index) + '1' + pattern.Substring(index+1);
+        WildcardBinary(patternWithZero, results);
+        WildcardBinary(patternWithOne, results);
     }
 
     /// <summary>
